46850e5b5369579df62bfa3d8ceacab6a6d9ce08
Shinnbeegpp <marfajonard17@gmail.com>
2026-09-15 23:12:48 +0800

    navigation bar style

--- apps/web/app/components/Navbar.tsx
+++ apps/web/app/components/Navbar.tsx
@@ -2,14 +2,30 @@ import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav>
-      <Link href="/">Home</Link>
-      {" | "}
-      <Link href="/builder">Build & Test</Link>
-      {" | "}
-      <Link href="/rate-my-pc">Rate My PC</Link>
-      {" | "}
-      <Link href="/results">Results</Link>
-    </nav>
+    <header className="navbar">
+      <div className="navbar-container">
+        <Link href="/" className="navbar-brand">
+          PC Performance Lab
+        </Link>
+
+        <nav className="navbar-links">
+          <Link href="/" className="navbar-link">
+            Home
+          </Link>
+
+          <Link href="/builder" className="navbar-link">
+            Build & Test
+          </Link>
+
+          <Link href="/rate-my-pc" className="navbar-link">
+            Rate My PC
+          </Link>
+
+          <Link href="/results" className="navbar-link">
+            Results
+          </Link>
+        </nav>
+      </div>
+    </header>
   );
 }
